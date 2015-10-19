98312dc12a5bc9c213a65560e0eca0a82854db33
olsoka03 <olsoka03@luther.edu>
2015-10-19 01:09:04 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,7 +1,11 @@
 addTask = function() {
   ul = document.querySelector("#list");
   li = document.createElement("li");
-  document.querySelector("#priority");
   check = document.createElement("checkbox");
+  document.querySelector("#new");
+  item = document.createTextNode("#new").value;
   
+  li.appendChild(check);
+  li.appendChild(item);
+  ul.appendChild(li);
 }
